db19167350df11ea85331b9e20506651d158988b
Syed Hussain <tekken482@hotmail.com>
2022-10-16 14:30:59 -0600

    welcome page

--- public/js/app.js
+++ public/js/app.js
@@ -5344,7 +5344,23 @@ __webpack_require__(/*! ./bootstrap */ "./resources/js/bootstrap.js");
 
 
 window.Alpine = alpinejs__WEBPACK_IMPORTED_MODULE_0__["default"];
-alpinejs__WEBPACK_IMPORTED_MODULE_0__["default"].start();
+alpinejs__WEBPACK_IMPORTED_MODULE_0__["default"].start(); // When the user scrolls down 80px from the top of the document, resize the navbar's padding and the logo's font size
+
+window.onscroll = function () {
+  animate_shrink_navbar();
+};
+
+function animate_shrink_navbar() {
+  if (document.body.scrollTop > 80 || document.documentElement.scrollTop > 80) {
+    document.getElementById("navbar").style.padding = "20px 10px 10px";
+    document.getElementById("logo").style.fontSize = "25px";
+    document.getElementById("navbar").style.opacity = "1.0";
+  } else {
+    document.getElementById("navbar").style.padding = "20px 10px 10px";
+    document.getElementById("navbar").style.opacity = "0.9";
+    document.getElementById("logo").style.fontSize = "35px";
+  }
+}
 
 /***/ }),
 
--- resources/js/app.js
+++ resources/js/app.js
@@ -5,3 +5,22 @@ import Alpine from 'alpinejs';
 window.Alpine = Alpine;
 
 Alpine.start();
+
+
+// When the user scrolls down 80px from the top of the document, resize the navbar's padding and the logo's font size
+window.onscroll = function() {
+    animate_shrink_navbar()
+    };
+
+function animate_shrink_navbar() {
+if (document.body.scrollTop > 80 || document.documentElement.scrollTop > 80) {
+    document.getElementById("navbar").style.padding = "20px 10px 10px";
+    document.getElementById("logo").style.fontSize = "25px";
+    document.getElementById("navbar").style.opacity = "1.0";
+} else {
+    document.getElementById("navbar").style.padding = "20px 10px 10px";
+    document.getElementById("navbar").style.opacity = "0.9";
+    document.getElementById("logo").style.fontSize = "35px";
+}
+}
+      
